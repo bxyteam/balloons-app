c91a21ffcc724f9b39ce651f0acbba617d331388
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2025-09-29 16:41:45 -0300

    fix runnable copy files into compile script

--- compile.sh
+++ compile.sh
@@ -11,10 +11,10 @@ echo "Creating runnable directory"
 mkdir -p runnable runnable/lib runnable/classes
 
 echo "Copying bindist/lib to runnable/lib"
-cp -r target/bindist/lib runnable/lib
+cp -r target/bindist/lib/* runnable/lib
 
 echo "Copying classes to runnable/classes"
-cp -r target/classes runnable/classes
+cp -r target/classes/* runnable/classes
 
 echo "Copying scripts to runnable"
 cp -r target/classes/scripts/. runnable
